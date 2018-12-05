469d7a3fa44cda12d0eee627898de516bfc44f44
Gianlu <altomanigianluca@gmail.com>
2018-12-05 14:23:03 +0100

    PlayerHandler methods

--- api/src/main/java/xyz/gianlu/librespot/api/PlayerHandler.java
+++ api/src/main/java/xyz/gianlu/librespot/api/PlayerHandler.java
@@ -4,18 +4,34 @@ import com.google.gson.JsonElement;
 import org.jetbrains.annotations.NotNull;
 import xyz.gianlu.librespot.api.server.AbsApiHandler;
 import xyz.gianlu.librespot.api.server.ApiServer;
+import xyz.gianlu.librespot.core.Session;
+import xyz.gianlu.librespot.player.Player;
 
 /**
  * @author Gianlu
  */
 public class PlayerHandler extends AbsApiHandler {
-    public PlayerHandler() {
+    private final Player player;
+
+    public PlayerHandler(@NotNull Session session) {
         super("player");
+        this.player = session.player();
     }
 
     @Override
-    protected @NotNull JsonElement handleRequest(ApiServer.@NotNull Request request) {
-        return string("OK");
+    protected @NotNull JsonElement handleRequest(ApiServer.@NotNull Request request) throws ApiServer.PredefinedJsonRpcException {
+        switch (request.getSuffix()) {
+            case "play":
+            case "stop":
+            case "pause":
+            case "next":
+            case "prev":
+            case "playPause":
+                return string("OK");
+            default:
+                throw ApiServer.PredefinedJsonRpcException.from(request, ApiServer.PredefinedJsonRpcError.METHOD_NOT_FOUND);
+
+        }
     }
 
     @Override
