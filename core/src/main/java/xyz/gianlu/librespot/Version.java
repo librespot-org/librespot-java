e7b0fabbe59a7e5f91f02e74f42bf66e6642234f
Gianlu <altomanigianluca@gmail.com>
2019-04-13 17:35:55 +0200

    Release 0.5.0

--- core/src/main/java/xyz/gianlu/librespot/Version.java
+++ core/src/main/java/xyz/gianlu/librespot/Version.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
  * @author Gianlu
  */
 public class Version {
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.5.0";
 
     @NotNull
     public static String versionString() {
