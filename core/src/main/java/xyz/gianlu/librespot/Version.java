4d7553700e33491c676d0434863f89cc650213fe
sJJdGG <25764288+sJJdGG@users.noreply.github.com>
2019-07-25 18:14:03 +0430

    Fix version in Version.java

--- core/src/main/java/xyz/gianlu/librespot/Version.java
+++ core/src/main/java/xyz/gianlu/librespot/Version.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
  * @author Gianlu
  */
 public class Version {
-    public static final String VERSION = "0.5.1";
+    public static final String VERSION = "0.6.1";
 
     @NotNull
     public static String versionString() {
