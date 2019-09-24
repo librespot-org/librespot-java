c086a95a8ce39920fa5eb3aaa9521626c9d7429c
Gianlu <altomanigianluca@gmail.com>
2019-09-24 16:47:52 +0200

    Updated version + refactored DealerClient

--- core/src/main/java/xyz/gianlu/librespot/dealer/DealerClient.java
+++ core/src/main/java/xyz/gianlu/librespot/dealer/DealerClient.java
@@ -9,6 +9,7 @@ import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import xyz.gianlu.librespot.common.NameThreadFactory;
 import xyz.gianlu.librespot.core.ApResolver;
 import xyz.gianlu.librespot.core.Session;
@@ -26,18 +27,15 @@ import java.util.concurrent.*;
 /**
  * @author Gianlu
  */
-public class DealerClient extends WebSocketListener implements Closeable {
+public class DealerClient implements Closeable {
     private static final JsonParser PARSER = new JsonParser();
     private static final Logger LOGGER = Logger.getLogger(DealerClient.class);
+    private final Looper looper = new Looper();
+    private final Session session;
     private final Map<MessageListener, List<String>> listeners = new HashMap<>();
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(new NameThreadFactory((r) -> "dealer-scheduler-" + r.hashCode()));
-    private final Session session;
-    private final Looper looper = new Looper();
-    private ScheduledFuture<?> lastScheduledPing;
-    private WebSocket ws;
-    private volatile boolean receivedPong = false;
-    private volatile boolean closed = true;
-    private ScheduledFuture<?> scheduledReconnect;
+    private ConnectionHolder conn;
+    private ScheduledFuture lastScheduledReconnection;
 
     public DealerClient(@NotNull Session session) {
         this.session = session;
@@ -48,103 +46,9 @@ public class DealerClient extends WebSocketListener implements Closeable {
      * Creates a new WebSocket client. <b>Intended for internal use only!</b>
      */
     public void connect() throws IOException, MercuryClient.MercuryException {
-        this.ws = session.client().newWebSocket(new Request.Builder()
+        conn = new ConnectionHolder(session, new Request.Builder()
                 .url(String.format("wss://%s/?access_token=%s", ApResolver.getRandomDealer(), session.tokens().get("playlist-read")))
-                .build(), this);
-    }
-
-    @Override
-    public void onOpen(@NotNull WebSocket webSocket, Response response) {
-        LOGGER.debug(String.format("Dealer connected! {host: %s}", response.request().url().host()));
-        closed = false;
-        if (scheduledReconnect != null) scheduledReconnect.cancel(true);
-
-        lastScheduledPing = scheduler.scheduleAtFixedRate(() -> {
-            sendPing();
-            receivedPong = false;
-
-            scheduler.schedule(() -> {
-                if (lastScheduledPing == null || lastScheduledPing.isCancelled()) return;
-
-                if (!receivedPong) {
-                    LOGGER.warn("Did not receive ping in 3 seconds. Reconnecting...");
-                    reconnect();
-                }
-                receivedPong = false;
-            }, 3, TimeUnit.SECONDS);
-        }, 0, 30, TimeUnit.SECONDS);
-    }
-
-    private void scheduleReconnection() {
-        if (scheduledReconnect != null) scheduledReconnect.cancel(true);
-        scheduledReconnect = scheduler.schedule(this::reconnect, 10, TimeUnit.SECONDS);
-    }
-
-    private void reconnect() {
-        if (closed) return;
-
-        closed = true;
-        if (ws != null) ws.cancel();
-        if (lastScheduledPing != null) {
-            lastScheduledPing.cancel(true);
-            lastScheduledPing = null;
-        }
-
-        if (scheduledReconnect != null) {
-            scheduledReconnect.cancel(true);
-            scheduledReconnect = null;
-        }
-
-        try {
-            connect();
-        } catch (IOException | MercuryClient.MercuryException ex) {
-            LOGGER.error("Failed reconnecting! Retrying in 10 seconds...", ex);
-            scheduleReconnection();
-        }
-    }
-
-    private void sendPing() {
-        ws.send("{\"type\":\"ping\"}");
-    }
-
-    @Override
-    public void onFailure(@NotNull WebSocket ws, @NotNull Throwable t, Response response) {
-        if (t instanceof SocketException) {
-            if (closed) {
-                LOGGER.error("Failed reconnecting! Retrying in 10 seconds...", t);
-                scheduleReconnection();
-            } else {
-                LOGGER.warn("An exception occurred. Reconnecting...", t);
-                reconnect();
-            }
-            return;
-        }
-
-        LOGGER.error("Unexpected failure when handling message!", t);
-    }
-
-    @Override
-    public void onMessage(@NotNull WebSocket webSocket, @NotNull String text) {
-        JsonObject obj = PARSER.parse(text).getAsJsonObject();
-
-        waitForListeners();
-
-        MessageType type = MessageType.parse(obj.get("type").getAsString());
-        switch (type) {
-            case MESSAGE:
-                handleMessage(obj);
-                break;
-            case REQUEST:
-                handleRequest(obj);
-                break;
-            case PONG:
-                receivedPong = true;
-                break;
-            case PING:
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown message type for " + type);
-        }
+                .build());
     }
 
     private void waitForListeners() {
@@ -243,10 +147,41 @@ public class DealerClient extends WebSocketListener implements Closeable {
 
     @Override
     public void close() {
-        closed = true;
-        ws.close(1000, null);
+        if (conn != null) {
+            ConnectionHolder tmp = conn; // Do not trigger connectionInvalided()
+            conn = null;
+            tmp.close();
+        }
 
+        if (lastScheduledReconnection != null) {
+            lastScheduledReconnection.cancel(true);
+            lastScheduledReconnection = null;
+        }
+
+        scheduler.shutdown();
         listeners.clear();
+    }
+
+    /**
+     * Called when the {@link ConnectionHolder} has been closed and cannot be used no more for a connection.
+     */
+    private void connectionInvalided() {
+        if (lastScheduledReconnection != null && !lastScheduledReconnection.isDone())
+            throw new IllegalStateException();
+
+        conn = null;
+
+        LOGGER.trace("Scheduled reconnection attempt in 10 seconds...");
+        lastScheduledReconnection = scheduler.schedule(() -> {
+            lastScheduledReconnection = null;
+
+            try {
+                connect();
+            } catch (IOException | MercuryClient.MercuryException ex) {
+                LOGGER.error("Failed reconnecting, retrying...", ex);
+                connectionInvalided();
+            }
+        }, 10, TimeUnit.SECONDS);
     }
 
     public interface MessageListener {
@@ -277,6 +212,103 @@ public class DealerClient extends WebSocketListener implements Closeable {
         @Override
         public void close() {
             shouldStop = true;
+        }
+    }
+
+    private class ConnectionHolder implements Closeable {
+        private final WebSocket ws;
+        private boolean closed = false;
+        private boolean receivedPong = false;
+        private ScheduledFuture lastScheduledPing;
+
+        ConnectionHolder(@NotNull Session session, @NotNull Request request) {
+            ws = session.client().newWebSocket(request, new WebSocketListenerImpl());
+        }
+
+        private void sendPing() {
+            ws.send("{\"type\":\"ping\"}");
+        }
+
+        @Override
+        public void close() {
+            if (closed) {
+                ws.cancel();
+            } else {
+                closed = true;
+                ws.close(1000, null);
+            }
+
+            if (lastScheduledPing != null) {
+                lastScheduledPing.cancel(false);
+                lastScheduledPing = null;
+            }
+
+            if (conn == ConnectionHolder.this)
+                connectionInvalided();
+        }
+
+        private class WebSocketListenerImpl extends WebSocketListener {
+
+            @Override
+            public void onOpen(@NotNull WebSocket ws, @NotNull Response response) {
+                LOGGER.debug(String.format("Dealer connected! {host: %s}", response.request().url().host()));
+                if (closed) {
+                    LOGGER.fatal("I wonder what happened here... Terminating.");
+                    return;
+                }
+
+                lastScheduledPing = scheduler.scheduleAtFixedRate(() -> {
+                    sendPing();
+                    receivedPong = false;
+
+                    scheduler.schedule(() -> {
+                        if (lastScheduledPing == null || lastScheduledPing.isCancelled()) return;
+
+                        if (!receivedPong) {
+                            LOGGER.warn("Did not receive ping in 3 seconds. Reconnecting...");
+                            close();
+                            return;
+                        }
+
+                        receivedPong = false;
+                    }, 3, TimeUnit.SECONDS);
+                }, 0, 30, TimeUnit.SECONDS);
+            }
+
+            @Override
+            public void onMessage(@NotNull WebSocket ws, @NotNull String text) {
+                JsonObject obj = PARSER.parse(text).getAsJsonObject();
+
+                waitForListeners();
+
+                MessageType type = MessageType.parse(obj.get("type").getAsString());
+                switch (type) {
+                    case MESSAGE:
+                        handleMessage(obj);
+                        break;
+                    case REQUEST:
+                        handleRequest(obj);
+                        break;
+                    case PONG:
+                        receivedPong = true;
+                        break;
+                    case PING:
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown message type for " + type);
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull WebSocket ws, @NotNull Throwable t, @Nullable Response response) {
+                if (t instanceof SocketException) {
+                    LOGGER.warn("An exception occurred. Reconnecting...", t);
+                    close();
+                    return;
+                }
+
+                LOGGER.error("Unexpected failure when handling message!", t);
+            }
         }
     }
 }
