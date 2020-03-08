a0fb61e815abee763076f10e93d4c8eaff2a1cef
Gianlu <altomanigianluca@gmail.com>
2020-03-08 16:05:19 +0100

    Publish first snapshot

--- .travis/load_gpg.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ev
-
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
-  echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-fi
--- .travis/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ev
-
-if [[ -z "${TRAVIS_TAG}" ]]; then
-  mvn deploy -Dmaven.deploy.skip=true -B -U -Pdebug
-else
-  mvn deploy --settings .maven.xml -B -U -Prelease
-fi
