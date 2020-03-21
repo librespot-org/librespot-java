178c892fd55ca6ebfcdf24ce0f5123e9b9348a41
devgianlu <altomanigianluca@gmail.com>
2020-03-21 09:41:46 +0100

    Moved before_deploy + deploy Maven for master or dev only

--- .travis/before_deploy.sh
+++
@@ -1,4 +0,0 @@
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
-  echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-fi
