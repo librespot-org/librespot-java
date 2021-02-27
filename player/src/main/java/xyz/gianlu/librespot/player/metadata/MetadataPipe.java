514334848f8e898abd4baee37219e980c5340b62
Gianlu <altomanigianluca@gmail.com>
2021-02-27 11:51:17 +0100

    Add null terminator at the end of data when sending though metadata pipe (#317)

--- player/src/main/java/xyz/gianlu/librespot/player/metadata/MetadataPipe.java
+++ player/src/main/java/xyz/gianlu/librespot/player/metadata/MetadataPipe.java
@@ -61,8 +61,8 @@ public final class MetadataPipe {
         if (out == null) out = new FileOutputStream(file);
 
         if (payload != null) {
-            out.write(String.format("<item><type>%s</type><code>%s</code><length>%d</length>\n<data encoding=\"base64\">%s</data></item>\n", type, code,
-                    payload.length, new String(Base64.getEncoder().encode(payload), StandardCharsets.UTF_8)).getBytes(StandardCharsets.UTF_8));
+            out.write(String.format("<item><type>%s</type><code>%s</code><length>%d</length>\n<data encoding=\"base64\">%s\0</data></item>\n", type, code,
+                    payload.length, Base64.getEncoder().encodeToString(payload)).getBytes(StandardCharsets.UTF_8));
         } else {
             out.write(String.format("<item><type>%s</type><code>%s</code><length>0</length></item>\n", type, code).getBytes(StandardCharsets.UTF_8));
         }
